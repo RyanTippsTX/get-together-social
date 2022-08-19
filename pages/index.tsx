efc03d917f34d79419bdeff95287f77f78fd4761
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-08-19 17:35:59 -0500

    "under construction" landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -23,35 +23,22 @@ const Home: NextPage = () => {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">GetTogether.social</a>!
+          Welcome to <a href="https://gettogether.social">GetTogether.social</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
+        {/* <p className={styles.description}>
+          {
+            'Create a sharable landing page for you next party or private event with GetTogether.social'
+          }
+        </p> */}
+
+        <p className={styles.constructionWorker}>{'👷🏻‍♂️'}</p>
+        <p className={styles.underConstruction}>{"⚠️ We're currently under construction! ⚠️"}</p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
+          <a href="https://github.com/RyanTippsTX/get-together-social" className={styles.card}>
+            <h2>Source Code &rarr;</h2>
+            <p>Check out our progress on GitHub.</p>
           </a>
         </div>
       </main>
