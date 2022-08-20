3997c1f901a21ce5e626e13cbdb96e5a6bd77cc7
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-08-19 22:51:54 -0500

    hide vercel footer on 'under construction' page

--- pages/index.tsx
+++ pages/index.tsx
@@ -43,7 +43,7 @@ const Home: NextPage = () => {
         </div>
       </main>
 
-      <footer className={styles.footer}>
+      {/* <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
-      </footer>
+      </footer> */}
     </div>
   );
 };
