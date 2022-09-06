98eb474dd3de4f1060a662538872a6256e72b029
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-06 01:03:48 -0500

    define preliminary theme in Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    // screens: {
+    // sm: '640px',
+    // md: '768px',
+    // lg: '1024px',
+    // xl: '1280px',
+    // '2xl': '1536px',
+    // },
+    extend: {
+      fontFamily: {
+        // sans: ['Proxima Nova', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        // https://coolors.co/1c1c1c-ededed-fdfdfd-ffdf3d-388201-fff7a3-ffe0eb
+        black: '#1C1C1C',
+        gray: '#EDEDED',
+        white: '#FDFDFD',
+        lemon: '#FFDF3D',
+        'lemon-leaf': '#388201',
+        lemonade: '#FFF7A3',
+        'pink-lemonade': '#FFE0EB',
+      },
+    },
   },
   plugins: [],
 };
