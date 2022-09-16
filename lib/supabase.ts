986f3c959e2140b9bc107d3abf82cbaa7685afe2
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-16 00:07:47 -0500

    refactor supabase util to typescript

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,5 +1,9 @@
 import { createClient } from '@supabase/supabase-js';
 
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL || !process.env.NEXT_PUBLIC_SUPABASE_API_KEY) {
+  throw Error('Missing Supabase environment variable(s)');
+}
+
 // Create a single supabase client for interacting with your database
 export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
