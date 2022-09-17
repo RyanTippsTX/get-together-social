86dad819402f488bc9d9af439436eea095758897
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-17 02:21:34 -0500

    remove large screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,7 +20,7 @@ module.exports = {
       xs: '400px',
       sm: '640px',
       md: '768px',
-      lg: '1024px',
+      // lg: '1024px',
       // xl: '1280px',
       // '2xl': '1536px',
     },
