825f145a2becbd83bb257fbabfc9bedbcfe879ed
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-19 01:59:15 -0500

    created spinner component

---
+++ components/Spinner.tsx
@@ -0,0 +1,12 @@
+export default function Spinner() {
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className="spinner-border inline-block h-8 w-8 animate-spin rounded-full border-4"
+        role="status"
+      >
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    </div>
+  );
+}
