5df7ed338a4d9b94613e69db3cac66b3229bd34b
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-19 00:48:31 -0500

    add to marketing page list event suggestions

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ function Hero() {
                 'get-together',
                 'Friendsgiving',
                 'office party',
+                'game night',
                 'family reunion',
                 'barbeque',
                 'open mic night',
