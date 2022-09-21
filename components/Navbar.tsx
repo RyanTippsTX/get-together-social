0504454923f1effed998c75641701602149baff5
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-21 16:07:50 -0500

    refactor NavBar subcomponents and include Avatar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,49 +1,39 @@
 import { Brand } from './Brand';
+import { OptionsButton } from './OptionsButton';
+import { Avatar } from './Avatar';
 import { useAuth } from '../lib/auth';
+import { useProfile } from '../lib/profile';
 
 export function Navbar() {
-  return (
-    <div className="navbar border-b-[1px] border-zinc-200 bg-white">
-      <div className="container mx-auto">
-        <div className="flex-1">
-          <Brand />
-        </div>
-        <div className="flex-none">
-          <div className="dropdown dropdown-end">
-            <div tabIndex={0} className="btn btn-square btn-ghost">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-5 w-5 stroke-current"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-                ></path>
-              </svg>
-            </div>
+  const {
+    session,
+    user,
+    loading: sessionLoading,
+    signOut,
+    signInWithEmail,
+    signInWithGoogle,
+  } = useAuth();
+  const { profile, loading: profileLoading } = useProfile();
 
-            <ul
-              tabIndex={0}
-              className="menu menu-compact dropdown-content rounded-box mt-3 w-auto bg-white p-2 shadow"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  {/* <span className="badge">New</span> */}
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+  const navLeft = (
+    <div className="flex-1">
+      <Brand />
+    </div>
+  );
+
+  const navRight = (
+    <div className="flex-none">
+      <OptionsButton />
+      <Avatar avatarUrl={profile?.avatar_url} displayName={profile?.display_name} />
+    </div>
+  );
+
+  return (
+    <div className="border-b-[1px] border-zinc-200 bg-white">
+      <div className="container mx-auto ">
+        <div className="navbar ">
+          {navLeft}
+          {navRight}
         </div>
       </div>
     </div>
---
+++ components/OptionsButton.tsx
@@ -0,0 +1,39 @@
+export function OptionsButton() {
+  return (
+    <div className="dropdown dropdown-end">
+      <div tabIndex={0} className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block h-5 w-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+          ></path>
+        </svg>
+      </div>
+
+      <ul
+        tabIndex={0}
+        className="menu menu-compact dropdown-content rounded-box mt-3 w-auto bg-white p-2 shadow"
+      >
+        <li>
+          <a className="justify-between">
+            Profile
+            {/* <span className="badge">New</span> */}
+          </a>
+        </li>
+        <li>
+          <a>Settings</a>
+        </li>
+        <li>
+          <a>Logout</a>
+        </li>
+      </ul>
+    </div>
+  );
+}
