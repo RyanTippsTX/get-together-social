fa6aa43df5cc6b3659b9d08af93a614fbf09bc3e
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-26 17:53:00 -0500

    remove medium size display

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,7 +21,7 @@ module.exports = {
     screens: {
       xs: '400px',
       sm: '640px',
-      md: '768px',
+      // md: '768px',
       // lg: '1024px',
       // xl: '1280px',
       // '2xl': '1536px',
