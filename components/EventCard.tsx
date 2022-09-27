6252f5cf04ac52fb77b7c94b003ab0eb2b92eb1b
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-27 15:06:40 -0500

    add link icon to event card copy link button

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -53,9 +53,25 @@ export function EventCard({ event }: { event: Event }) {
               // Alert the copied text
               alert('Copied the text: ' + eventUrl);
             }}
-            className="btn btn-primary"
+            className="btn btn-primary flex"
           >
-            Link
+            <div className=" pr-1">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={2}
+                stroke="currentColor"
+                className="h-5 w-5"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244"
+                />
+              </svg>
+            </div>
+            Copy Link
           </button>
           <button className="btn btn-primary">Edit</button>
           <button
