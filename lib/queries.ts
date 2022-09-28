66751c6f958a2ea2132fa3f4e4861ffff2213892
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-09-27 21:22:41 -0500

    refactor queries with match filter

--- lib/queries.ts
+++ lib/queries.ts
@@ -8,7 +8,8 @@ export async function getEvent(url_code: string) {
   return await supabase
     .from('events')
     .select('*, hosts (*)')
-    .eq('url_code', url_code)
+    .match({ url_code })
+    // .eq('url_code', url_code)
     // .eq('url_string', url_string.toLowerCase())  // omit for now
     .single();
 }
@@ -16,21 +17,21 @@ export async function getEventViewCount(event_id: string) {
   return await supabase
     .from('page_visits')
     .select('*', { count: 'exact', head: true })
-    .eq('event_id', event_id);
+    .match({ event_id });
 }
 
 export async function getContributions(event_id: string) {
   return await supabase
     .from('contributions')
     .select('*, guests( * )')
-    .eq('event_id', event_id)
+    .match({ event_id })
     .order('created_at', { ascending: true });
 }
 export async function getContribution(event_id: string) {
   return await supabase
     .from('contributions')
     .select('*, guests( * )')
-    .eq('event_id', event_id)
+    .match({ event_id })
     .order('created_at', { ascending: true })
     .single();
 }
