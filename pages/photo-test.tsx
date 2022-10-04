be476fa73bc2cb998ae99b9a5471452769358c63
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-04 18:34:12 -0500

    created temp file for photo upload testing

---
+++ pages/photo-test.tsx
@@ -0,0 +1,17 @@
+import Layout from '../components/layout';
+
+export default function PhotoTest() {
+  return (
+    <Layout>
+      <h1 className="text-3xl font-bold">Photo Test</h1>
+      <p>
+        * App bio
+        <br />
+        * Short bio of RMT
+        <br />
+        * Link to LinkedIn, GitHub
+        <br />* Donate button
+      </p>
+    </Layout>
+  );
+}
