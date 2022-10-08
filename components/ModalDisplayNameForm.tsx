f10501441e49dc34f456387540d788133be27c40
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-07 19:00:09 -0500

    add cancel button to display name modal

--- components/ModalDisplayNameForm.tsx
+++ components/ModalDisplayNameForm.tsx
@@ -81,6 +81,17 @@ export function ModalDisplayNameForm({
           {/* submit button */}
           <div className="modal-action">
             <button
+              onClick={(e) => {
+                e.stopPropagation();
+                // console.log('clicked card');
+                closeModal();
+              }}
+              className="btn btn-ghost"
+            >
+              Cancel
+            </button>
+
+            <button
               type="submit"
               // className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
               className="btn"
