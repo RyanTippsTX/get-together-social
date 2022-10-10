26b11808b89de0359e1ae5a8aa3f1a56b1d043ac
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-10 18:06:51 -0500

    feat - app-wide loading splashscreen, accessible through context API

---
+++ lib/appLoading.tsx
@@ -0,0 +1,31 @@
+// this context provides access to the state used by the 'AppLoading' indicator component
+import { createContext, useContext, useEffect, useState, FormEvent } from 'react';
+
+interface AppLoadingContextInterface {
+  appLoading: boolean;
+  setAppLoading: Function;
+}
+export const AppLoadingContext = createContext<AppLoadingContextInterface | undefined>(undefined);
+
+export function AppLoadingProvider({ ...props }) {
+  const [appLoading, setAppLoading] = useState(false);
+
+  return (
+    <AppLoadingContext.Provider
+      value={{
+        appLoading,
+        setAppLoading,
+      }}
+      {...props}
+    />
+  );
+}
+
+// hook for using app-wide Profile state & Profile mutation functions
+export function useAppLoading() {
+  const context: any = useContext(AppLoadingContext);
+  if (context === undefined) {
+    throw new Error('useAppLoading must be used within an AppLoadingProvider');
+  }
+  return context;
+}
