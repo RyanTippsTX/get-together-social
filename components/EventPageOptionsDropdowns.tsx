322de9573a291e3639e8f29e03550a31b376b866
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-18 17:36:57 -0500

    feat - functionality for guest user to update their display name

--- components/EventPageOptionsDropdowns.tsx
+++ components/EventPageOptionsDropdowns.tsx
@@ -1,9 +1,10 @@
 import Link from 'next/link';
-import { MouseEventHandler } from 'react';
+import { MouseEventHandler, useState } from 'react';
 import { useAuth } from '../lib/auth';
 import { useRouter } from 'next/router';
 import { useEventState } from '../lib/eventState';
 import { useGuestAuth } from '../lib/guestAuth';
+import { ModalGuestDisplayNameForm } from './modals/ModalGuestDisplayNameForm';
 
 export function HostNavbarOptionsDropdown() {
   const router = useRouter();
@@ -86,48 +87,63 @@ export function GuestNavbarOptionsDropdown() {
   const { guest, setGuest, guestList, setGuestList } = useGuestAuth();
   const { signOut } = useAuth();
   const { event, setEvent } = useEventState();
+  const [guestDisplayNameModalOpen, setGuestDisplayNameModalOpen] = useState<boolean>(false);
   return (
-    <div className="dropdown dropdown-end flex">
-      <div tabIndex={0} className="btn btn-square btn-ghost flex flex-col">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="inline-block h-5 w-5 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
-          ></path>
-        </svg>
-      </div>
-
-      <ul
-        tabIndex={0}
-        className="menu menu-compact dropdown-content rounded-box absolute top-full right-0 mt-3 min-w-max bg-white p-2 shadow"
-      >
-        <li>
-          <button
-            onClick={() => {
-              setGuest(null);
-            }}
+    <>
+      <ModalGuestDisplayNameForm
+        isOpen={guestDisplayNameModalOpen}
+        closeModal={() => {
+          setGuestDisplayNameModalOpen(false);
+        }}
+      />
+      <div className="dropdown dropdown-end flex">
+        <div tabIndex={0} className="btn btn-square btn-ghost flex flex-col">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block h-5 w-5 stroke-current"
           >
-            Sign Out of Guest Account
-          </button>
-        </li>
-        <li>
-          <button>Edit My Display Name</button>
-        </li>
-        <li>
-          <button>Delete All My Contributions</button>
-        </li>
-        {/* Future option: allow account merge in case a user accidentally contributes under multiple names */}
-        {/* <li>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </div>
+
+        <ul
+          tabIndex={0}
+          className="menu menu-compact dropdown-content rounded-box absolute top-full right-0 mt-3 min-w-max bg-white p-2 shadow"
+        >
+          <li>
+            <button
+              onClick={() => {
+                setGuest(null);
+              }}
+            >
+              Sign Out of Guest Account
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={() => {
+                setGuestDisplayNameModalOpen(true);
+              }}
+            >
+              Edit My Display Name
+            </button>
+          </li>
+          <li>
+            <button>Delete All My Contributions</button>
+          </li>
+          {/* Future option: allow account merge in case a user accidentally contributes under multiple names */}
+          {/* <li>
           <button>Merge With Another Guest </button> 
         </li> */}
-      </ul>
-    </div>
+        </ul>
+      </div>
+    </>
   );
 }
---
+++ components/modals/ModalGuestDisplayNameForm.tsx
@@ -0,0 +1,142 @@
+import { Modal } from './Modal';
+import { updateGuest } from '../../lib/queries';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { useGuestAuth } from '../../lib/guestAuth';
+import { useEventState } from '../../lib/eventState';
+import { useAppLoading } from '../../lib/appLoading';
+import { Host, Event, Guest, Contribution, Contributions } from '../../lib/queries.types';
+
+export function ModalGuestDisplayNameForm({
+  isOpen,
+  closeModal,
+}: // onSuccess,
+{
+  isOpen: boolean;
+  closeModal: Function;
+  // onSuccess: Function;
+}) {
+  const { guest, setGuest, guestList, setGuestList } = useGuestAuth();
+  const { event } = useEventState();
+  const { appLoading, setAppLoading } = useAppLoading();
+
+  const returningGuestUser = guestList?.find((g) => g.guest_id === guest!.guest_id);
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<{ display_name: string }>({
+    defaultValues: {
+      display_name: returningGuestUser!.display_name,
+    },
+  });
+  const onSubmit: SubmitHandler<{ display_name: string }> = ({ display_name }) => {
+    if (!event) return;
+    // console.log('Form submission data:', data);
+    closeModal();
+    (async () => {
+      setAppLoading(true);
+
+      await updateGuest({ guest_id: guest!.guest_id, display_name });
+      setAppLoading(false);
+    })();
+  };
+
+  // guestList && console.log([...guestList?.values()]);
+  console.log(guestList);
+
+  return (
+    <Modal {...{ isOpen }} {...{ closeModal }}>
+      {/* <p>edit your display name !!</p> */}
+      {/* <h3 className="text-lg font-bold">Edit your display name:</h3>
+      <p className="py-4">
+        Youve been selected for a chance to get one year of subscription to use Wikipedia for free!
+      </p> */}
+      {/* // Main Form Body */}
+
+      <form autoComplete="off" noValidate onSubmit={handleSubmit(onSubmit)} className="">
+        <div className="card-body ">
+          <h2 className="card-title">Edit Your Display Name</h2>
+          <p>
+            Your Display Name is your guest log-in username and is shown with any contributions you
+            make in the {event?.contributions_custom_title || 'Guest Contribtutons'}.
+          </p>
+
+          {/* input title */}
+
+          {/* <label htmlFor="display_name" className="block text-sm font-medium text-gray-700">
+            Display Name
+          </label> */}
+          <input
+            list="guests"
+            // type="text"
+            {...register('display_name', {
+              required: { value: true, message: 'Required' },
+              maxLength: { value: 60, message: 'Too long' },
+              // pattern: { value: /[A-Za-z]/i, message: 'Must contain a character' }, // this is done by default when field is required
+            })}
+            id="display_name"
+            placeholder="Your Name"
+            // autoComplete="nope"
+            className="form-input mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+          />
+          <datalist id="guests">
+            {guestList &&
+              Array.from(guestList.keys()).map((display_name) => {
+                return (
+                  <option key={display_name as unknown as string} value={display_name}></option>
+                );
+              })}
+          </datalist>
+
+          <FormError message={errors.display_name?.message as string} />
+          <p className="mt-2 text-sm text-gray-500">
+            We reccommend using your full name, or any nickname that uniquely identifies you from
+            other guests. 🥸
+          </p>
+
+          {/* end of inputs */}
+
+          {/* action buttons */}
+          <div className="modal-action">
+            <button
+              onClick={(e) => {
+                e.stopPropagation();
+                // console.log('clicked button');
+                closeModal();
+              }}
+              className="btn btn-ghost"
+            >
+              Cancel
+            </button>
+
+            <button
+              type="submit"
+              // className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+              className="btn"
+            >
+              Save
+            </button>
+          </div>
+          {/* <h2 className="card-title">Hosts</h2>
+          <p>
+            Your Login is <a>here</a>
+          </p> */}
+        </div>
+      </form>
+    </Modal>
+  );
+}
+
+function FormError({ message }: { message: string | undefined | null }) {
+  if (message) {
+    return (
+      <div className="text-error px-1 pt-1 text-sm font-semibold">
+        {/* {errors.email?.message as string} */}
+        {message}
+      </div>
+    );
+  }
+  return <></>;
+}
