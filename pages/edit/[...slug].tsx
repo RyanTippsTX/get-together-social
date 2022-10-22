158184f36af7a9e6065e695b123a72e8f897582f
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-22 05:57:40 -0500

    stylize button color on new & edit event pages to website theme

--- pages/edit/[...slug].tsx
+++ pages/edit/[...slug].tsx
@@ -388,10 +388,7 @@ export default function Edit({ event }: { event: Event }) {
               </div>
               {/* submit button */}
               <div className="bg-gray-50 px-4 py-3 text-right sm:px-6">
-                <button
-                  type="submit"
-                  className="inline-flex justify-center rounded-md border border-transparent bg-indigo-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
+                <button type="submit" className="btn btn-primary">
                   Save
                 </button>
               </div>
