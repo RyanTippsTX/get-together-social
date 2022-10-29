c0859868b2bf2d4b2099683ccaf5e37aac9c7186
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-29 04:06:57 -0500

    apply REST standards to stats route

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -2,25 +2,28 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import supabase from '../../lib/supabase';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  // res.status(200).json({ name: 'John Doe' });
+  if (req.method === 'GET') {
+    const [
+      { count: hostCount },
+      { count: guestCount },
+      { count: eventCount },
+      { count: visitCount },
+    ] = await Promise.all([
+      supabase.from('hosts').select('*', { count: 'estimated', head: true }),
+      supabase.from('guests').select('*', { count: 'estimated', head: true }),
+      supabase.from('events').select('*', { count: 'estimated', head: true }),
+      supabase.from('page_visits').select('*', { count: 'estimated', head: true }),
+    ]);
 
-  const [
-    { count: hostCount },
-    { count: guestCount },
-    { count: eventCount },
-    { count: visitCount },
-  ] = await Promise.all([
-    supabase.from('hosts').select('*', { count: 'estimated', head: true }),
-    supabase.from('guests').select('*', { count: 'estimated', head: true }),
-    supabase.from('events').select('*', { count: 'estimated', head: true }),
-    supabase.from('page_visits').select('*', { count: 'estimated', head: true }),
-  ]);
-
-  res.status(200).json({
-    hostCount,
-    guestCount,
-    eventCount,
-    visitCount,
-  });
+    res.status(200).json({
+      hostCount,
+      guestCount,
+      eventCount,
+      visitCount,
+    });
+  } else {
+    // Reject any other HTTP method
+    res.status(405).send('Method Not Allowed');
+  }
 };
 export default handler;
