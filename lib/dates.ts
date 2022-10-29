4f036a338287bafd4c3f874adb02a8a55e3605c7
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2022-10-29 05:55:51 -0500

    bugfix - format dates correctly as UTC, omit any timezone conversion

--- lib/dates.ts
+++ lib/dates.ts
@@ -1,4 +1,26 @@
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
-  return date.toDateString();
+
+  const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+  const months = [
+    'Jan',
+    'Feb',
+    'Mar',
+    'Apr',
+    'May',
+    'Jun',
+    'Jul',
+    'Aug',
+    'Sep',
+    'Oct',
+    'Nov',
+    'Dec',
+  ];
+
+  const dayOfWeek = days[date.getUTCDay()];
+  const month = months[date.getUTCMonth()];
+  const dayOfMonth = date.getUTCDate();
+  const year = date.getUTCFullYear();
+
+  return `${dayOfWeek} ${month} ${dayOfMonth}, ${year}`;
 }
