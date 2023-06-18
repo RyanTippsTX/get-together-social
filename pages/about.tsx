47bf93f09a38ef881806056f2a325eeda5b6c90e
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2023-06-18 09:23:01 -0500

    remove link to personal site on about page

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,20 +3,12 @@ import Layout from '../components/layout';
 export default function About() {
   return (
     <Layout>
-      <div className="xs:min-h-[45rem] mx-auto flex min-h-[35rem] max-w-sm flex-col  gap-6 pt-10">
+      <div className="xs:min-h-[45rem] mx-auto flex min-h-[35rem] max-w-lg flex-col  gap-6 pt-10">
         <h1 className=" text-center text-5xl font-bold tracking-tight text-zinc-700 ">{'About'}</h1>
         <h2 className=" text-justify text-xl  tracking-tight text-zinc-700 ">
-          {"Hi, I'm "}
-          <a
-            className="font-medium text-blue-600 hover:underline dark:text-blue-500"
-            href="https://ryantipps.com/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Ryan Tipps
-          </a>
-          , developer and creator of <span className="font-semibold">GetTogether.social</span>, a
-          web application built with Next.js, Supabase, & Tailwind. Check out the source code on{' '}
+          Hi, I'm Ryan Tipps, developer and creator of{' '}
+          <span className="font-semibold">GetTogether.social</span>, a web application built with
+          Next.js, Supabase, & Tailwind. Check out the source code on{' '}
           <a
             className="font-medium text-blue-600 hover:underline dark:text-blue-500"
             href="https://github.com/RyanTippsTX/get-together-social/tree/dev"
@@ -36,7 +28,6 @@ export default function About() {
           </a>
           . Then hire me!
         </h2>
-        {/* GetTogether.social launched on September X, 2022 */}
       </div>
     </Layout>
   );
