6a1ad424bdb2c6f09d8b155ac29c83f062a413e0
Ryan Tipps <89077432+RyanTippsTX@users.noreply.github.com>
2023-06-18 09:23:27 -0500

    revise width container on legal page

--- pages/legal/index.tsx
+++ pages/legal/index.tsx
@@ -3,14 +3,13 @@ import Layout from '../../components/layout';
 export default function Legal() {
   return (
     <Layout>
-      <div className="xs:min-h-[45rem] mx-auto flex min-h-[35rem] max-w-sm flex-col items-center gap-6 pt-10">
+      <div className="xs:min-h-[45rem] mx-auto flex min-h-[35rem] max-w-lg flex-col items-center gap-6 pt-10">
         <h1 className="text-center text-5xl font-bold tracking-tight text-zinc-700 ">{'Legal'}</h1>
         <h2 className="text-justify text-xl  tracking-tight text-zinc-700 ">
           {
             'GetTogether.social is still in development. Our Privacy Policy, Terms of Use, and Cookie Policy are coming soon.'
           }
         </h2>
-        {/* GetTogether.social launched on September X, 2022 */}
       </div>
     </Layout>
   );
